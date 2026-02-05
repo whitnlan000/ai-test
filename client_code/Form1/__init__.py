60d6564a1f6e9444f842446278adc5efc24a15f3
mysticclan236@gmail.com <mysticclan236@gmail.com>
2026-02-05 18:09:54 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -287,7 +287,7 @@ Math Helper 3 exemplifies the complex interplay between technology, education, a
       output.append(i)
   
   
-    for _ in range(100):  # generate 100 words
+    for _ in range(1000): 
       key = prefix_words[-2] + " " + prefix_words[-1]
       next_word = predict(key)
       output.append(next_word)
